ae71d864a121755f366e2494c8b95a73b695fdd0
jsh1147 <jsh1147@naver.com>
2024-11-18 11:47:51 +0900

    fix: 오타 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
