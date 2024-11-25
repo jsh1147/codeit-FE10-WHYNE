d90e9476fb89ad181a75acd3c6c7e98b9840427e
swim-kim <ksoo5386@naver.com>
2024-11-26 02:44:55 +0900

    feat: 와인 컴포넌트 ui

--- components/myProfile/MyWines.css.ts
+++ components/myProfile/MyWines.css.ts
@@ -8,11 +8,19 @@ export const WineListContainer = styled.div`
     flex-direction: column;
     gap:8px;
 `;
+
+export const WineItemWrapper = styled.div`
+    width:100%;
+    height:270px;
+    display:flex;
+    align-items: flex-end;
+`;
 export const WineItem = styled.div`
-    display: inline-flex;
+    width:100%;
+    height: 228px;
     padding: 24px 40px 30px 40px;
     align-items: flex-start;
-    gap: 20px;
+    gap: 40px;
     flex-shrink: 0;
     border-radius: 16px;
     border: 1px solid var(--gray-300);
@@ -20,4 +28,66 @@ export const WineItem = styled.div`
     ${media.mobile`
         padding: 16px 20px;
     `}
+    display:flex;
+`;
+
+export const TotalCount = styled.div`
+    position:relative;
+    bottom:30px;
+    color: var(--purple-100);
+    text-align: right;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 24px;
+    ${media.mobile`
+        font-size: 12px;
+    `}
+`;
+export const WineImage = styled(Image)`
+    width:76px;
+    height:270px;
+    position:relative;
+    bottom: 65px;
+`
+export const WineInfoWrapper = styled.div`
+    width:300px;
+    display: flex;
+    flex-direction: column;
+    gap:13px;
+`;
+export const WineNameWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:20px;
+`;
+export const WineNameText = styled.div`
+    color: var(--gray-800);
+    font-size: 30px;
+    font-weight: 600;
+`;
+export const WineRegionText = styled.div`
+    color: var(--gray-500);
+    font-weight: 400;
+    line-height: 26px;
+`;
+export const Price = styled.div`
+    display: inline-flex;
+    height: 37px;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+`;
+export const PriceText = styled.div`
+    color: var(--purple-100);
+    padding: 8px 15px;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 26px; 
+    border-radius: 12px;
+    background: var(--purple-10);
+`;
+export const KebapIcon = styled(Image)`
+    width:20px;
+    height:20px;
 `;
--- components/myProfile/MyWines.tsx
+++ components/myProfile/MyWines.tsx
@@ -2,13 +2,28 @@ import * as S from './MyWines.css';
 import StarIcon from '@/public/icons/icon_star.svg';
 import KebapIcon from '@/public/icons/icon_kebap.svg';
 import { useEffect, useState, useRef, useCallback } from 'react';
-
+import { TotalCount } from './MyReviews.css';
+import WineImage from '@/public/images/wineimg_example.png';
 export default function MyWines() {
     return (
         <S.WineListContainer>
-            <S.WineItem>
-
-            </S.WineItem>
+            <S.TotalCount>총 18개</S.TotalCount>
+            <S.WineItemWrapper>
+                <S.WineItem>
+                    <S.WineImage src={ WineImage } alt="와인이미지"/>
+                    <S.WineInfoWrapper>
+                        <S.WineNameWrapper>
+                            <S.WineNameText>Sentinel Carbernet Sauvignon 2016</S.WineNameText>
+                            <S.WineRegionText>Western Cape, South Africa</S.WineRegionText>
+                        </S.WineNameWrapper>
+                        <S.Price>
+                            <S.PriceText>₩ 64,99990</S.PriceText>
+                        </S.Price>
+                    </S.WineInfoWrapper>
+                    <S.KebapIcon src={KebapIcon} alt="더보기" />
+                </S.WineItem>
+            </S.WineItemWrapper>
+            
         </S.WineListContainer>
     );
 }
