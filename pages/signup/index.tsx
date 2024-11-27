dfbf0011c9b995b86e140ae24990afcff8769e84
jsh1147 <jsh1147@naver.com>
2024-11-27 22:06:19 +0900

    feat: 회원가입/로그인 페이지 일부 구현
    - 메타 데이터 추가
    - 유저 접근 시 랜딩 페이지로 이동

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,23 +1,45 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useUser } from '@/store/UserContext';
 import SignUpForm from '@/components/login/SignUpForm';
 import * as S from '@/styles/auth.css';
 
 export default function SignUp() {
+  const { replace } = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (user) replace('/');
+  }, [user, replace]);
+
   return (
-    <S.Main>
-      <S.HiddenH1>WINE 회원가입 페이지</S.HiddenH1>
-      <S.Section>
-        <S.LogoLink href="/" aria-label="랜딩 페이지 바로가기">
-          <S.Logo aria-label="WINE 로고" />
-        </S.LogoLink>
-        <SignUpForm />
-        <S.SignUpText>
-          계정이 이미 있으신가요?
-          <S.MoveLink href="/login" replace>
-            로그인하기
-          </S.MoveLink>
-        </S.SignUpText>
-      </S.Section>
-    </S.Main>
+    <>
+      <Head>
+        <title>회원가입 - WINE</title>
+        <meta
+          name="description"
+          content="간단한 회원가입으로 WINE 서비스를 시작해 보세요."
+        />
+      </Head>
+      {!isLoading && !user && (
+        <S.Main>
+          <S.HiddenH1>WINE 회원가입 페이지</S.HiddenH1>
+          <S.Section>
+            <S.LogoLink href="/" aria-label="랜딩 페이지 바로가기">
+              <S.Logo aria-label="WINE 로고" />
+            </S.LogoLink>
+            <SignUpForm />
+            <S.SignUpText>
+              계정이 이미 있으신가요?
+              <S.MoveLink href="/login" replace>
+                로그인하기
+              </S.MoveLink>
+            </S.SignUpText>
+          </S.Section>
+        </S.Main>
+      )}
+    </>
   );
 }
 
