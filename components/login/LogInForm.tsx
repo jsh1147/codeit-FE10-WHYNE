977b37855c4b087460f896c5c7ce0d01e2110fbf
jsh1147 <jsh1147@naver.com>
2024-11-28 06:15:41 +0900

    feat: 로그인/회원가입 폼 틀 구현
    - 로그인/회원가입 폼 기본 구조 구현

--- components/login/LogInForm.tsx
+++ components/login/LogInForm.tsx
@@ -1,3 +1,34 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
 export default function LogInForm() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>();
+
+  const logInSubmit: SubmitHandler<LoginFormData> = (data, event) => {
+    event?.preventDefault();
+  };
+
+  return (
+    <form method="post" onSubmit={handleSubmit(logInSubmit)}>
+      <label htmlFor="email">이메일</label>
+      <input id="email" placeholder="이메일 입력" {...register('email')} />
+      <span>{errors.email?.message}</span>
+      <label htmlFor="password">비밀번호</label>
+      <input
+        id="password"
+        placeholder="비밀번호 입력"
+        {...register('password')}
+      />
+      <span>{errors.password?.message}</span>
+      <button type="submit">로그인</button>
+    </form>
+  );
 }
--- components/login/SignUpForm.tsx
+++ components/login/SignUpForm.tsx
@@ -1,3 +1,46 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface SignUpFormData {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
 export default function SignUpForm() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormData>();
+
+  const SignUpSubmit: SubmitHandler<SignUpFormData> = (data, event) => {
+    event?.preventDefault();
+  };
+
+  return (
+    <form method="post" onSubmit={handleSubmit(SignUpSubmit)}>
+      <label htmlFor="email">이메일</label>
+      <input id="email" placeholder="wine@email.com" {...register('email')} />
+      <span>{errors.email?.message}</span>
+      <label htmlFor="nickname">닉네임</label>
+      <input id="nickname" placeholder="wine" {...register('nickname')} />
+      <span>{errors.nickname?.message}</span>
+      <label htmlFor="password">비밀번호</label>
+      <input
+        id="password"
+        placeholder="영문, 숫자 포함 8자 이상"
+        {...register('password')}
+      />
+      <span>{errors.password?.message}</span>
+      <label htmlFor="passwordConfirmation">비밀번호 확인</label>
+      <input
+        id="passwordConfirmation"
+        placeholder="비밀번호 확인"
+        {...register('passwordConfirmation')}
+      />
+      <span>{errors.passwordConfirmation?.message}</span>
+      <button type="submit">회원가입</button>
+    </form>
+  );
 }
