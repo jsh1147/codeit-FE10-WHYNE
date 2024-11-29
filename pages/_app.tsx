f0683af558a8d4396e094fec5fdf90f5cf6f81ee
Suzy-Lee <>
2024-11-29 23:51:58 +0900

    fix: 빌드시 파일명 에러 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import localFont from 'next/font/local';
 import { UserProvider } from '@/store/UserContext';
 import Layout from '@/components/layout/Layout';
-import GlobalStyle from '@/styles/globalStyles';
+import GlobalStyles from '@/styles/GlobalStyles';
 
 const pretendard = localFont({
   src: '../public/fonts/PretendardVariable.woff2',
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: MyAppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <GlobalStyle />
+      <GlobalStyles />
       <ProviderComponent>
         <LayoutComponent>
           <Component className={pretendard.className} {...pageProps} />
